b8bcd3ae94e200e572d796365b04fce69a910fc7
YuJungin <Jungin@YJI.local>
2017-05-08 08:57:06 +0900

    5차 세미나 파트원 수정 완

--- seminar_5/seminar_5_sql/routes/partone.js
+++ seminar_5/seminar_5_sql/routes/partone.js
@@ -66,7 +66,39 @@ router.post('/save', function(req, res){
         }
       });
     }
+  });
+});
+//수정
+router.post('/edit', function(req, res){
+  let query = 'select * from student where id = ?';
+  pool.getConnection(function(err, connection){
+    if(err) console.log('getConection error:', err);
+    else {
+      connection.query(query , req.body.id, function(err, data){ // injection 방어 : +가 아니라 , 를 추가
+        if(err){
+          console.log("first query err: ", err);
+          connection.release(); //pool에 반납ㄴ한다. connection 이면 end()
+        }
+        else{
+          if(data.length==0) res.status(403).send("존재하지 않는 아이디입니다.");
+          else{'update user set point = point - ? where email = ?';
+            let query2 = 'update student set name = ?, part = ? where id = ?';
+            connection.query(query2, [req.body.name, req.body.part, req.body.id], function(err, result){
+                  if(err){
+                    console.log('second query err', err);
+                    connection.relase();
+                  }
+                  else{
+                    res.status(202).send("수정완료");
+                  }
+                  connection.release();
+            });
+          }
+        }
+      });
+    }
 
   });
 });
+
  module.exports = router;
